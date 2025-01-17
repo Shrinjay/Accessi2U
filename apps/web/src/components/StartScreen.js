335d01690a945ebda18809713197e17f2f5331d8
cwibach <cwibach@uwaterloo.ca>
2025-01-17 18:50:12 -0500

    existing files added to repo

---
+++ apps/web/src/components/StartScreen.js
@@ -0,0 +1,10 @@
+import React, { useCallback } from "react";
+import {Button} from "./components/ui/button"
+
+export default function StartScreen() {
+    return (
+        <div>
+            <Button>Let's Go!</Button>
+        </div>
+    );
+}
