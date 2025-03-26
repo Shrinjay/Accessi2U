779b3437de43ac2a4ff1fbd628f1fbb3fa86d514
Shrinjay Mukherjee <shrinjaymukherjee@gmail.com>
2025-03-25 22:32:16 -0400

    fix: viewports

--- apps/web/src/components/SelectLocations.tsx
+++ apps/web/src/components/SelectLocations.tsx
@@ -102,119 +102,79 @@ export default function SelectLocations() {
   return (
     <Flex height="100vh" width="100vw" bg="gray.100">
       {/* Left Panel */}
-      <Stack direction="row" pt={12}>
-        <MapLegend />
-        <MapTutorial />
-      </Stack>
+      <MapLegend />
+      <MapTutorial />
 
       {menuOpen ? (
-        <>
-          <Box
-            p="6"
-            bg="white"
-            boxShadow="lg"
-            flexDirection="column"
-            sx={{
-              left: 0,
-              zIndex: 1001,
-              width: { xs: '100%', md: '50%', lg: '30%' },
-            }}
-          >
-            <Tabs isFitted variant={'enclosed'} colorScheme="purple">
-              <TabList>
-                <Tab>Find Route</Tab>
-                <Tab>Map View</Tab>
-              </TabList>
-              <TabPanels>
-                <TabPanel>
-                  <Heading fontSize={'3xl'} mt="10px" mb="10px" mr={2}>
-                    Select Route Start/End
-                  </Heading>
-                  <Box width="100%">
-                    <Text fontSize={'2xl'} fontWeight="bold" mb={2} mt="20px">
-                      Your Location
-                    </Text>
-                    <Select
-                      styles={theme}
-                      isClearable
-                      isDisabled={isLoading}
-                      value={startPoint}
-                      options={options}
-                      isOptionDisabled={(option) => option.isDisabled}
-                      onChange={setStart}
-                      placeholder="DWE 1431"
-                      aria-errormessage="*Required"
-                      aria-invalid={true}
-                    />
-                    <Text fontSize="xs" fontWeight="thin" mt={0} ml={2}>
-                      *Required
-                    </Text>
-
-                    <Text fontSize={'2xl'} fontWeight="bold" mb={2} mt="30px">
-                      Final Location
-                    </Text>
-                    <Select
-                      styles={theme}
-                      isClearable
-                      isDisabled={isLoading}
-                      isOptionDisabled={(option) => option.isDisabled}
-                      value={endPoint}
-                      options={options}
-                      onChange={setEnd}
-                      placeholder="DWE 1432"
-                    />
-                    <Text fontSize="xs" fontWeight="thin" mt={0} ml={2}>
-                      *Required
-                    </Text>
-
-                    <Text fontSize={['2xl']} fontWeight="bold" mb={2} color="brand.500" mt="10px">
-                      {' '}
-                      Select your Preferences
-                    </Text>
-
-                    <HStack>
-                      <Checkbox isChecked={accessible} onChange={(e) => setAccessible(e.target.checked)} />
-                      <Text fontSize={['md']} mt="5px" fontFamily="body">
-                        Elevator only
-                      </Text>
-                    </HStack>
-
-                    <Button
-                      size="lg"
-                      colorScheme="brand"
-                      bg="purple.500"
-                      color="white"
-                      fontSize="18px"
-                      py={4}
-                      borderRadius="8px"
-                      fontWeight="bold"
-                      isDisabled={!completedInfo || isGeneratingPath}
-                      onClick={pathSelected}
-                      isLoading={isGeneratingPath}
-                      mt={6}
-                      _hover={{ bg: '#4D2161' }}
-                      _active={{ bg: '#4D2161' }}
-                    >
-                      Confirm Route
-                    </Button>
-                  </Box>
-                </TabPanel>
-                <TabPanel>
-                  <Heading fontSize={'3xl'} mt="10px" mb="10px" mr={2}>
-                    Select Floor
-                  </Heading>
+        <Box
+          p="6"
+          width={{ xs: 'full', sm: 'full', md: '50%', lg: '30%' }}
+          bg="white"
+          boxShadow="lg"
+          flexDirection="column"
+          sx={{
+            left: 0,
+            zIndex: 1001,
+          }}
+        >
+          <Tabs isFitted variant={'enclosed'} colorScheme="purple">
+            <TabList>
+              <Tab>Find Route</Tab>
+              <Tab>Map View</Tab>
+            </TabList>
+            <TabPanels>
+              <TabPanel>
+                <Heading fontSize={'3xl'} mt="10px" mb="10px" mr={2}>
+                  Select Route Start/End
+                </Heading>
+                <Box width="100%">
+                  <Text fontSize={'2xl'} fontWeight="bold" mb={2} mt="20px">
+                    Your Location
+                  </Text>
                   <Select
                     styles={theme}
                     isClearable
                     isDisabled={isLoading}
-                    value={selectedFloor}
-                    options={floorOptions}
-                    onChange={setSelectedFloor}
-                    placeholder="DWE_01"
+                    value={startPoint}
+                    options={options}
+                    isOptionDisabled={(option) => option.isDisabled}
+                    onChange={setStart}
+                    placeholder="DWE 1431"
+                    aria-errormessage="*Required"
+                    aria-invalid={true}
                   />
                   <Text fontSize="xs" fontWeight="thin" mt={0} ml={2}>
                     *Required
                   </Text>
+
+                  <Text fontSize={'2xl'} fontWeight="bold" mb={2} mt="30px">
+                    Final Location
+                  </Text>
+                  <Select
+                    styles={theme}
+                    isClearable
+                    isDisabled={isLoading}
+                    isOptionDisabled={(option) => option.isDisabled}
+                    value={endPoint}
+                    options={options}
+                    onChange={setEnd}
+                    placeholder="DWE 1432"
+                  />
+                  <Text fontSize="xs" fontWeight="thin" mt={0} ml={2}>
+                    *Required
+                  </Text>
+
+                  <Text fontSize={['2xl']} fontWeight="bold" mb={2} color="brand.500" mt="10px">
+                    {' '}
+                    Select your Preferences
+                  </Text>
+
+                  <HStack>
+                    <Checkbox isChecked={accessible} onChange={(e) => setAccessible(e.target.checked)} />
+                    <Text fontSize={['md']} mt="5px" fontFamily="body">
+                      Elevator only
+                    </Text>
+                  </HStack>
 
                   <Button
                     size="lg"
@@ -225,36 +185,73 @@ export default function SelectLocations() {
                     py={4}
                     borderRadius="8px"
                     fontWeight="bold"
-                    mt={6}
-                    isDisabled={selectedFloor == null}
-                    onClick={() => setMenuOpen(false)}
+                    isDisabled={!completedInfo || isGeneratingPath}
+                    onClick={pathSelected}
                     isLoading={isGeneratingPath}
+                    mt={6}
                     _hover={{ bg: '#4D2161' }}
                     _active={{ bg: '#4D2161' }}
                   >
-                    View Floor
+                    Confirm Route
                   </Button>
-                </TabPanel>
-              </TabPanels>
-            </Tabs>
-            {/* TODO: Lol maybe one day */}
-            {/* <HStack>
+                </Box>
+              </TabPanel>
+              <TabPanel>
+                <Heading fontSize={'3xl'} mt="10px" mb="10px" mr={2}>
+                  Select Floor
+                </Heading>
+                <Select
+                  styles={theme}
+                  isClearable
+                  isDisabled={isLoading}
+                  value={selectedFloor}
+                  options={floorOptions}
+                  onChange={setSelectedFloor}
+                  placeholder="DWE_01"
+                />
+                <Text fontSize="xs" fontWeight="thin" mt={0} ml={2}>
+                  *Required
+                </Text>
+
+                <Button
+                  size="lg"
+                  colorScheme="brand"
+                  bg="purple.500"
+                  color="white"
+                  fontSize="18px"
+                  py={4}
+                  borderRadius="8px"
+                  fontWeight="bold"
+                  mt={6}
+                  isDisabled={selectedFloor == null}
+                  onClick={() => setMenuOpen(false)}
+                  isLoading={isGeneratingPath}
+                  _hover={{ bg: '#4D2161' }}
+                  _active={{ bg: '#4D2161' }}
+                >
+                  View Floor
+                </Button>
+              </TabPanel>
+            </TabPanels>
+          </Tabs>
+          {/* TODO: Lol maybe one day */}
+          {/* <HStack>
                     <Checkbox isChecked={indoors} onChange={(e) => setIndoors(e.target.checked)} />
                     <Text fontSize={['md']} mt="5px" fontFamily="body">
                       Indoor only
                     </Text>
                   </HStack> */}
 
-            {/* TODO: LMFAO probably never */}
-            {/* <HStack>
+          {/* TODO: LMFAO probably never */}
+          {/* <HStack>
                     <Checkbox />
                     <Text fontSize={['md']} mt="5px" fontFamily="body">
                       Hands-free
                     </Text>
                   </HStack> */}
 
-            {/* TODO: Maybe with cookies, unlikely*/}
-            {/* <Button
+          {/* TODO: Maybe with cookies, unlikely*/}
+          {/* <Button
                   size="md"
                   colorScheme="yellow"
                   bg="yellow.500"
@@ -267,8 +264,7 @@ export default function SelectLocations() {
                 >
                   Save
                 </Button> */}
-          </Box>
-        </>
+        </Box>
       ) : (
         <></>
       )}
--- apps/web/src/components/features/Home/Home.tsx
+++ apps/web/src/components/features/Home/Home.tsx
@@ -28,6 +28,7 @@ const HomeContainer = styled.div`
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  min-height: 100vh;
+  height: 100vh;
+  width: 100vw;
   background: linear-gradient(to right, #434343, #000000);
 `;
